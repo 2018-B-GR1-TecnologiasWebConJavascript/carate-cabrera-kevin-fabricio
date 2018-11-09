24180d034eb15dc9a9e43e6aa35aeff1db25d67c
Kevinfcc10 <kevin_01fcc@hotmail.com>
2018-11-09 17:21:28 -0500

    Observables y promesas

--- 02-Typescript/02-observables.js
+++ 02-Typescript/02-observables.js
@@ -1,8 +1,44 @@
 //02-observables.ts
 const rxjs = require('rxjs');
-const numeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+const map = require('rxjs/operators').map;
+const distinct = require('rxjs/operators').distinct;
+const concat = require('rxjs/operators').concat;
+const numeros$ = rxjs.of(1, 'Kevin', true, 'Kevin', 1, { nombre: 'kevin' }, [1, 2, 3], 1);
 console.log(numeros$);
-numeros$.subscribe((ok) => {
+const promesita = (correcto) => {
+    return new Promise((resolve, reject) => {
+        if (correcto) {
+            resolve(':)');
+        }
+        else {
+            reject(':(');
+        }
+    });
+};
+const promesitaNoOk$ = rxjs.from(promesita(false));
+const promesitaOk$ = rxjs.from(promesita(true));
+/*
+promesita$
+    .subscribe(
+        (ok)=>{
+            console.log('En promesita', ok);
+        },
+        (error)=> {
+            console.log('error en promesita', error);
+        },
+        ()=>{
+            console.log('completado')
+        }
+    )
+*/
+numeros$
+    .pipe(concat(promesitaNoOk$), concat(promesitaOk$))
+    .pipe(distinct(), map((valorActual) => {
+    return {
+        data: valorActual
+    };
+}))
+    .subscribe((ok) => {
     console.log('En ok', ok);
 }, (error) => {
     console.log('Error', error);
--- 02-Typescript/02-observables.ts
+++ 02-Typescript/02-observables.ts
@@ -2,14 +2,73 @@
 
 declare var require: any;
 
-const rxjs = require('rxjs');
 
-const numeros$ = rxjs.of(1,2,3,4,5,6);
+
+const rxjs = require('rxjs');
+const map = require('rxjs/operators').map;
+const distinct = require('rxjs/operators').distinct;
+const concat = require('rxjs/operators').concat;
+
+const numeros$ = rxjs.of(
+    1,
+    'Kevin',
+    true,
+    'Kevin',
+    1,
+    {nombre:'kevin'},
+    [1,2,3],
+    1);
 
 console.log(numeros$);
 
+const promesita = (correcto) => {
+    return new Promise(
+        (resolve,reject) => {
+            if(correcto){
+                resolve(':)')
+            }
+            else {
+                reject(':(')
+            }
+        }
+    )
+}
 
-numeros$.subscribe(
+
+const promesitaNoOk$ = rxjs.from(promesita(false));
+const promesitaOk$ = rxjs.from(promesita(true));
+
+/*
+promesita$
+    .subscribe(
+        (ok)=>{
+            console.log('En promesita', ok);
+        },
+        (error)=> {
+            console.log('error en promesita', error);
+        },
+        ()=>{
+            console.log('completado')
+        }
+    )
+*/
+
+numeros$
+    .pipe(
+        concat(promesitaNoOk$),
+        concat(promesitaOk$)
+    )
+    .pipe(
+        distinct(),
+        map(
+            (valorActual)=>{
+            return {
+                data:valorActual
+                };
+            }
+        )
+    )
+    .subscribe(
     (ok)=>{
         console.log('En ok', ok);
     },
@@ -20,3 +79,4 @@ numeros$.subscribe(
         console.log('Completado');
     }
 );
+
